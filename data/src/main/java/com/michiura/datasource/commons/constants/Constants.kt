bdf62fd253c10e041407bf3e86903c2cbdf2ffdb
fkmichiura <fkmichiura@gmail.com>
2023-05-28 11:45:22 -0300

    Moved api keys to gradle file

--- data/src/main/java/com/michiura/datasource/commons/constants/Constants.kt
+++ data/src/main/java/com/michiura/datasource/commons/constants/Constants.kt
@@ -1,14 +1,11 @@
 package com.michiura.datasource.commons.constants
 
 object Constants {
-    const val PUBLIC_API_KEY = "faebecefe77bcc0afe33eaf93269a580"
-    const val PRIVATE_API_KEY = "b48dce4c341e242a25fdbd2d7ef42d65e1b0eebc"
-
     const val DATE_PATTERN = "yyyy-MM-dd'T'HH:mm'Z'"
     const val MESSAGE_DIGEST_ALGORITHM = "MD5"
 
     const val TIMPESTAMP_QUERY_PARAM = "ts"
-    const val API_KEY_QUERY_PARAM = "apikey"
+    const val PUBLIC_API_KEY_QUERY_PARAM = "apikey"
     const val MD5_HASH_QUERY_PARAM = "hash"
 
     const val MD5_DIGEST_SIGN = 1
